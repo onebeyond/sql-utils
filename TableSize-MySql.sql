c73fc560b1b862fd7b95e9e8496bca1dec531a36
Nick Skliar-Davies <nick.skliar-davies@one-beyond.com>
2026-07-06 17:30:35 +0100

    sys: replace specific DB tables

--- TableSize-MySql.sql
+++ TableSize-MySql.sql
@@ -3,5 +3,5 @@ SELECT
     ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)",
     TABLE_ROWS AS "Row Count"
     FROM information_schema.TABLES
-    WHERE table_schema = 'yaypi'
+    WHERE table_schema = 'INSERT DATABASE NAME HERE'
     ORDER BY (data_length + index_length) DESC;
