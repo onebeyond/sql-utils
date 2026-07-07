37c8ccab11d902a88d4f8f78271c4100e3eae600
Nick Skliar-Davies <nick.skliar-davies@one-beyond.com>
2026-07-07 16:43:22 +0100

    refactor: use variables

--- TableSize-MySql.sql
+++ TableSize-MySql.sql
@@ -1,7 +1,9 @@
+SET @table_schema = 'INSERT DATABASE NAME HERE';
+
 SELECT
     table_name AS "Table",
     ROUND(((data_length + index_length) / 1024 / 1024), 2) AS "Size (MB)",
     TABLE_ROWS AS "Row Count"
     FROM information_schema.TABLES
-    WHERE table_schema = 'INSERT DATABASE NAME HERE'
+    WHERE table_schema = @table_schema
     ORDER BY (data_length + index_length) DESC;
